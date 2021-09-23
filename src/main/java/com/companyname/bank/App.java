24f52394ed4d672000be4d58f07c0219a88a5549
adithyarepo1 <89850810+adithyarepo1@users.noreply.github.com>
2021-09-23 18:19:41 +0530

    github hook change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "TEST COMMIT 1 new1" );
+        System.out.println( "TEST COMMIT 1 git hub hook test " );
     }
 }
